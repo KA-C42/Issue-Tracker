8b4db07e7ee367d46f09a6b05c63a74038f00b0f
emSoTired <kacyCote42@gmail.com>
2026-05-05 16:06:37 -0700

    fix: type correction in migration

--- supabase/migrations/002_create_profiles.sql
+++ supabase/migrations/002_create_profiles.sql
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS profiles (
   deactivated_at timestamptz DEFAULT NULL
 
 );
+
+CREATE OR REPLACE FUNCTION create_new_user_profile()
+    RETURNS trigger AS $$
+    BEGIN
+      INSERT INTO profiles (id, username)
+        VALUES (NEW.id, 'user_' || substring(NEW.id::text from 1 for 6))
+        ON CONFLICT (username) DO UPDATE SET username = 'user_' || substring(NEW.id::text from 1 for 8 );
+    RETURN new;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER new_user
+    AFTER INSERT ON auth.users
+    FOR EACH ROW EXECUTE FUNCTION create_new_user_profile();
