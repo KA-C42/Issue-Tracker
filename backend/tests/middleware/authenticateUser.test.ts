93e897177fd49f6ed594036f433d31a692e27db4
emSoTired <kacyCote42@gmail.com>
2026-05-07 16:34:00 -0700

    test: add protected route test coverage

--- backend/tests/middleware/authenticateUser.test.ts
+++ backend/tests/middleware/authenticateUser.test.ts
@@ -1,9 +1,15 @@
 import { Application } from 'express'
 import { beforeEach, describe, it } from 'vitest'
 import request from 'supertest'
-import { User } from '../../src/types/db'
+import { Project, User } from '../../src/types/db'
 import createApp from '../../src/api/app'
-import { createTestUser } from '../integration/helpers/createTestRows'
+import {
+  createInvitation,
+  createTestComment,
+  createTestIssue,
+  createTestProject,
+  createTestUser,
+} from '../integration/helpers/createTestRows'
 import { createAuthToken } from '../integration/helpers/createAuthToken'
 import jwt from 'jsonwebtoken'
 
@@ -55,5 +61,72 @@ describe('authenticateUser middleware function', () => {
       .get(`/profiles/${user.id}`)
       .set('Authorization', `Bearer ${expiredToken}`)
       .expect(403)
+  })
+})
+
+describe('auth protected routes hit auth first', () => {
+  let app: Application
+  let user: User
+  let token: string
+  let project: Project
+
+  beforeEach(async () => {
+    app = createApp()
+    user = await createTestUser()
+  })
+
+  it('returns 401 if request a profile without auth provided', async () => {
+    await request(app)
+      .get(`/profiles/${user.id}`)
+      .expect(401)
+      .expect('Content-Type', /json/)
+  })
+
+  beforeEach(async () => {
+    token = createAuthToken(user.id)
+    project = await createTestProject(app, token)
+  })
+
+  it('returns 401 if request a project without auth provided', async () => {
+    await request(app)
+      .get(`/projects/${project.id}`)
+      .expect(401)
+      .expect('Content-Type', /json/)
+  })
+
+  it('returns 401 if request a contributor without auth provided', async () => {
+    await request(app)
+      .get(`/projects/${project.id}/contributors`)
+      .expect(401)
+      .expect('Content-Type', /json/)
+  })
+
+  it('returns 401 if request an issue without auth provided', async () => {
+    const issue = await createTestIssue(app, token, project.id)
+
+    await request(app)
+      .get(`/issues/${issue.id}`)
+      .expect(401)
+      .expect('Content-Type', /json/)
+  })
+
+  it('returns 401 if request a comment without auth provided', async () => {
+    const issue = await createTestIssue(app, token, project.id)
+    await createTestComment(app, token, issue.id)
+
+    await request(app)
+      .get(`/issues/${issue.id}/comments`)
+      .expect(401)
+      .expect('Content-Type', /json/)
+  })
+
+  it('returns 401 if request an invitation without auth provided', async () => {
+    const invitee = await createTestUser('new@m.m')
+    await createInvitation(app, token, invitee.id, project.id)
+
+    await request(app)
+      .get(`/invitations?receiver_id=${invitee.id}`)
+      .expect(401)
+      .expect('Content-Type', /json/)
   })
 })
