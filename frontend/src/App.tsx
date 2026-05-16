cdce1dc8e757b9eacb5dcfbc28e940b7a93f1127
emSoTired <kacyCote42@gmail.com>
2026-05-16 16:15:42 -0700

    chore: configure tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css'
+import './style.css'
 import HealthScreen from './pages/HealthScreen'
 
 function App() {
