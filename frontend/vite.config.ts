c21f53f7af887c829b895d2cac12697de3a58e14
emSoTired <kacyCote42@gmail.com>
2026-05-16 17:41:32 -0700

    chore: configure shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     // dev only: access backend paths via /api/backend-path instead of full url
     proxy: {
