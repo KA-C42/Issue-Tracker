cad676b0ea709be8e3166cd54b07752e87bcc1c9
emSoTired <kacyCote42@gmail.com>
2026-05-18 13:36:27 -0700

    chore: configure react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './style.css'
 import HealthScreen from './pages/HealthScreen'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
           <Route path="/" element={<h1>Hooray! You made it!</h1>} />
           <Route path="/health" element={<HealthScreen />} />
         </Routes>
       </Router>
-    </>
+    </QueryClientProvider>
   )
 }
 
